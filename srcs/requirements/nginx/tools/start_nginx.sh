a73b4b8f473d3b086efe5dc0cbb7fb3cd898de31
Isabel Tovar <itovarnuez@gmail.com>
2024-06-21 13:55:02 +0200

    first

--- srcs/requirements/nginx/tools/start_nginx.sh
+++ srcs/requirements/nginx/tools/start_nginx.sh
@@ -1,4 +1,10 @@
-echo "--------- NGINX START ---------"
+echo "--------- NGINX START ---------\n"
 
+# Permet d'afficher la version de nginx
 nginx -v
+
+
+# Impose à nginx de tourner en foreground plutôt qu’en background 
+# quelque soit ce qui a été paramétré dans le fichier de configuration.
+# Il est indispensable de faire tourner nginx en “foreground” sinon le container s’arrêtera à la fin du script.
 nginx -g 'daemon off;'
